0eacbe3c1706ce4b6dad40ac82dac388d417b3ef
fwio <624835339@qq.com>
2022-11-14 17:36:48 +0800

    Add notations and rearrange the structure.

--- scripts/rename.ts
+++ scripts/rename.ts
@@ -3,13 +3,24 @@ import { normalize } from 'node:path'
 import { argv } from 'node:process'
 import { performance } from 'node:perf_hooks'
 
+// arguments passed via CLI
 interface CLIArgs {
   [key: string]: string
 }
 
 renameFiles()
 
-// i.e. tsx rename.ts --dir ../foo/bar/ --postfix -c
+// main runner
+async function renameFiles() {
+  const options = parseArgv(argv)
+
+  const files = await getCurrentFilenames(options.dir)
+
+  await suffixAll(options.dir, files, options.suffix)
+}
+
+// parse arguments passed via CLI into a dictionary
+// i.e. tsx rename.ts --dir ../foo/bar/ --suffix -c
 function parseArgv(argv: string[]) {
   const copy = [...argv],
     options: CLIArgs = {}
@@ -22,29 +33,36 @@ function parseArgv(argv: string[]) {
   return options
 }
 
+// scan files under the given directory
 async function getCurrentFilenames(dir: string) {
-  try {
-    const files = await readdir(normalize(dir))
-    
-    console.log('Scanning files:\n')
-    files.forEach(file => console.log(file))
+  const result: string[] = []
 
-    return files
+  try {
+    console.log('Scanning files:\n')
+    const files = await readdir(normalize(dir))
+
+    files.forEach((file, i) => {
+      result.push(file)
+      console.log(`[${i}] ${file}`)
+    })
   } catch(e) {
     console.log(e)
   } finally {
     console.log('\nScanning completed.\n')
   }
+
+  return result
 }
 
-async function postfixAll(dir: string, files: string[], postfix: string) {
+// decorate all selected files with given suffix
+async function suffixAll(dir: string, files: string[], suffix: string) {
   console.log('Start renaming ...\n')
   const start = performance.now()
 
   files.forEach(async file => {
     const [name, ...ext] = file.split('.')
     const oldPath = normalize(`${dir}/${file}`),
-      newPath = normalize(`${dir}/${name}${postfix}.${ext.join('')}`)
+      newPath = normalize(`${dir}/${name}${suffix}.${ext.join('')}`)
 
     try {
       await rename(oldPath, newPath)
@@ -56,12 +74,4 @@ async function postfixAll(dir: string, files: string[], postfix: string) {
   const end = performance.now(),
     duration = (end - start) / 1000
   console.log(`Renamed totally ${files.length} files in ${duration} s.\n`)
-}
-
-async function renameFiles() {
-  const options = parseArgv(argv)
-
-  const files = await getCurrentFilenames(options.dir)
-
-  await postfixAll(options.dir, files as string[], options.postfix)
 }
